7b1282283c9ad3f5d55d9f4b08dc8198d8374320
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-05-22 19:15:32 +0530

    Implemented interface and type defintions for todo

---
+++ src/types/todo.ts
@@ -0,0 +1,20 @@
+// src/types/todo.ts - Type definitions for todo
+
+export type TodoPriority = 'low' | 'medium' | 'high';
+
+// Interface for todo item response obtained from edge function
+export interface Todo {
+  id: string;
+  title: string;
+  description?: string;
+  completed: boolean;
+  priority: TodoPriority;
+  updated_at: string; 
+  created_at: string;
+}
+
+export interface TodoFormValues {
+  title: string;
+  description?: string;
+  priority: TodoPriority;
+}
